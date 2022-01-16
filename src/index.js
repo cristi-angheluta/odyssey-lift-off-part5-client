d7cf836f4250002a19bcff72639e63e7ee45761b
Cristi ANGHELUTA <cristinel.angheluta@gmail.com>
2022-01-17 00:03:43 +0200

    Add a default value in case REACT_APP_API_URL is not defined

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_API_URL}`,
+  uri: `${process.env.REACT_APP_API_URL || 'http://localhost:4000'}`,
   cache: new InMemoryCache(),
 });
 
